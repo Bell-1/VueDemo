2fd843b251ff1d7d524571373343135043094942
贝云江 <byj@xingyunbooks.com>
2018-08-29 14:43:47 +0800

    fix first

---
+++ test/unit/specs/HelloWorld.spec.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import HuarongRoad from '@/router/HuarongRoad/HuarongRoad'
+
+describe('HuarongRoad', () => {
+  // 检查原始组件选项
+  it('has a created hook', () => {
+    expect(true).toBe(true)
+  })
+})
